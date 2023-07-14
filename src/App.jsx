936592410170f672c51f5f4e851820a0e3d41957
MD.Mahabub Rahman <100031330+Mahabub112@users.noreply.github.com>
2023-07-14 21:45:46 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import {createRoutesFromElements,createBrowserRouter, Route, RouterProvider, } from "react-router-dom";
-import Registration from "./pages/Registration";
-import Login from "./pages/Login";
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Registration/>} ></Route>
-      <Route path="/login" element={<Login/>} ></Route>
-      </>
-    )
-  );
+import React from 'react'
+import {createRoutesFromElements, createBrowserRouter,Route,RouterProvider} from "react-router-dom";
+import Registation from './pages/Registation';
+import Login from './pages/Login';
+import Forgotpassword from './pages/Forgotpassword';
+import Home from './pages/Home';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Message from './pages/Message';
+import RootLayout from './components/RootLayout';
 
-function App(){
-  return (
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-      <RouterProvider router={router} />
+    <Route path="/" element={<Registation />}  > </Route>
+    <Route path="/login" element={<Login />}  > </Route>
+    <Route path="/forgotpassword" element={<Forgotpassword />}  > </Route>
+    <Route path="/chatting" element={<RootLayout/>}>
+      <Route path="home" element={<Home />}  > </Route>
+      <Route path="message" element={<Message />}  > </Route>
+    </Route>
     </>
   )
+);
+const App = () => {
+  return (
+    <>
+       <ToastContainer />
+      <RouterProvider router={router} />
+    </>
+  ) 
 }
-export default App;
+
+export default App
---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfZEUruDjViPE_AMKMYVIewYatz_wHR8A",
+  authDomain: "firstchatting-ab315.firebaseapp.com",
+  projectId: "firstchatting-ab315",
+  storageBucket: "firstchatting-ab315.appspot.com",
+  messagingSenderId: "922996492616",
+  appId: "1:922996492616:web:09a69439f8590756068b7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
