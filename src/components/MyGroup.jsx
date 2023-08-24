10e666bb307f643ce707dcffb2a626b7cffdf201
Mahabub Rahman <mdmahabubrahman800@gmail.com>
2023-08-24 23:15:22 +0600

    error

--- src/components/MyGroup.jsx
+++ src/components/MyGroup.jsx
@@ -31,6 +31,17 @@ const style = {
     boxShadow: 24,
     p: 4,
   };
+const style2 = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const MyGroup = () => {
     const db = getDatabase();
@@ -39,9 +50,12 @@ const MyGroup = () => {
     let userData = useSelector((state)=> state.loggedUser.loginUser)
     let [reqList,setReqList] = useState([])
     let [myGruoupRecList,setmyGruoupRecList] = useState([])
+    let [myMemberList,setMyMemberList] = useState([])
     const [open, setOpen] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
     // const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const handleClose2 = () => setOpen2(false);
   
     const handleOpen = (group) =>{
         const friendRequestRef = ref(db, 'grouprequest/');
@@ -61,6 +75,39 @@ const MyGroup = () => {
         setOpen(true);
     
       }
+
+      
+    const handleOpen2 = (member) =>{
+        const friendRequestRef = ref(db, 'members');
+        onValue(friendRequestRef, (snapshot) => {
+        let arr = []
+        snapshot.forEach(item=>{
+            if(userData.uid == item.val().admin && item.val().groupid == member.groupid){
+                arr.push({...item.val(), membersid: item.key});
+            }
+            
+        })
+        setMyMemberList(arr)
+        });
+        
+        setOpen2(true);
+    
+      }
+    
+    //   const handleOpen2 = (member) => {
+    //     const groupRef = ref(db, 'members');
+    //     onValue(groupRef, (snapshot) => {
+    //         let arr = []
+    //         snapshot.forEach(item=>{
+    //             if(userData.uid == item.val().admin && item.val().groupid == member.groupid){
+    //                 arr.push({...item.val(), memberid:item.key})
+    //             }
+    //         })
+    //         setMyMemberList(arr)
+    //     });
+    //     setOpen2(true)
+     
+    //   };
     
     useEffect(()=>{
         const friendRequestRef = ref(db, 'groups/');
@@ -98,6 +145,14 @@ const MyGroup = () => {
         remove(ref(db, "grouprequest/" + item.groupid));
 
     }
+    let handleMemberAccept = (item)=>{
+        console.log(item)
+        set(push(ref(db, 'members/')), {
+            ...item
+          }).then(()=>{
+            remove(ref(db, "grouprequest/" + item.groupid));
+          })
+    }
 
   return (
 
@@ -120,7 +175,7 @@ const MyGroup = () => {
                 </div>
                     <div className="button">
                     <Button onClick={()=>handleOpen(item)} size='small' variant="contained">Request</Button>
-                    <Button size='small' variant="contained" color='success'>Member</Button>
+                    <Button onClick={()=>handleOpen2(item)} size='small' variant="contained" color='success'>Member</Button>
                     </div>
             </div>
         </>
@@ -160,8 +215,8 @@ const MyGroup = () => {
                         </Typography>
                         {" — want's to join your group"}
                         <br/>
-                        <Button size='small' variant="contained" color='success'>Accept</Button>
-                    <Button onClick={()=>handleGroupDelete(item)}  size='small' variant="contained" color='success'>Delete</Button>
+                        <Button onClick={()=>handleMemberAccept(item)} size='small' variant="contained" color='success'>Accept</Button>
+                        <Button onClick={()=>handleGroupDelete(item)}  size='small' variant="contained" color='error'>Delete</Button>
                         </React.Fragment>
                     }
                     />
@@ -176,6 +231,59 @@ const MyGroup = () => {
           </Typography>
         </Box>
       </Modal>
+{/* group request end */}
+
+
+         <Modal
+        open={open2}
+        onClose={handleClose2}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={style2}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+            Group Members List
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {myMemberList.map(item=>(
+                
+                <>
+                     <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                    primary={item.username}
+                    secondary={
+                        <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                          
+
+                        </Typography>
+                        {" — want's to join your group"}
+                        <br/>
+                       
+                        {/* <Button onClick={()=>handleGroupDelete(item)}  size='small' variant="contained" color='error'>Remove</Button> */}
+                        </React.Fragment>
+                    }
+                    />
+                    
+                </ListItem>
+                <Divider variant="inset" component="li" />
+                </>
+            ))}
+               
+
+    </List>
+          </Typography>
+        </Box>
+        </Modal>
 
 </div>
     
