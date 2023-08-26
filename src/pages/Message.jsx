c26ecafc84211a94e89dbf6a5ddf26885b97698d
Mahabub Rahman <mdmahabubrahman800@gmail.com>
2023-08-26 10:52:43 +0600

    massenger design part-1

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import Group from '../components/Group';
+import FriendRequest from '../components/FriendRequest';
+import Friends from '../components/Friends';
+import MyGroup from '../components/MyGroup';
+import UserList from '../components/UserList';
+import BlockUser from '../components/BlockUser';
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <Grid container spacing={2}>
+    <Grid item xs={4}>
+      <div>
+         <Group/> 
+         <Friends/>
+      </div>
+    </Grid>
+    <Grid item xs={8}>
+      <div>
+       
+        <MyGroup/>
+      </div>
+    </Grid>
+  
+  
+  </Grid>
   )
 }
 
